955e225b819d1b62d6ca2d56e7f37c4e9da61dc9
woowang789 <wangjh789@gmail.com>
2023-02-26 10:55:00 +0900

    [design] add order page

--- src/main/java/com/samgyeobsal/controller/OrderController.java
+++ src/main/java/com/samgyeobsal/controller/OrderController.java
@@ -2,11 +2,12 @@ package com.samgyeobsal.controller;
 
 import com.samgyeobsal.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 /**
@@ -25,16 +26,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/web/order")
+@Slf4j
 public class OrderController {
 
-    private final OrderService service;
+    private final OrderService orderService;
 
     private String memberName = "isshosng";
 
     @RequestMapping(value="", method = RequestMethod.GET)
     public String orderForm(Model model){
-
         return "order/orderDetail";
-
     }
+
+    // 편딩 id로 펀딩 찾아서 해당 리워드들 모델에 넣어줌
+    @GetMapping("/step1/{fundingId}")
+    public String orderStep1Page(@PathVariable("fundingId") String fundingId){
+        return "order/order_step1";
+    }
+
+    // step2 페이지로 리다이렉트 하게끔
+    @PostMapping("/step1/{fundingId}")
+    public String orderStep1(
+            @PathVariable("fundingId") String fundingId,
+            RedirectAttributes redirect){
+
+//        redirect.addFlashAttribute();
+        return "redirect:/web/order/step2";
+    }
+
+    @GetMapping("/step2")
+    public String orderStep2Page(){
+        return "order/order_step2";
+    }
+
 }
