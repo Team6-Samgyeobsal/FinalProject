61f29b09e5d49d77a772a4268c98271ab4bccce4
Gyu Seok <115447023+thrbtjr@users.noreply.github.com>
2023-02-28 10:06:38 +0900

    [feat] modified db/ddl.sql

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,5 +1,17 @@
+drop table member cascade constraint purge;
+drop table category cascade constraint purge;
+drop table funding cascade constraint purge;
+drop table funding_product cascade constraint purge;
+drop table notice cascade constraint purge;
+drop table order_item cascade constraint purge;
+drop table orders cascade constraint purge;
+drop table payment_method cascade constraint purge;
+drop table reply cascade constraint purge;
+drop table the_hyundai cascade constraint purge;
+drop table competiton cascade constraint purge;
+
 -- 생성자 Oracle SQL Developer Data Modeler 21.2.0.183.1957
---   위치:        2023-02-27 17:54:33 KST
+--   위치:        2023-02-28 09:48:02 KST
 --   사이트:      Oracle Database 21c
 --   유형:      Oracle Database 21c
 
@@ -26,6 +38,37 @@ CREATE TABLE competiton (
 
 ALTER TABLE competiton ADD CONSTRAINT competiton_pk PRIMARY KEY ( cid );
 
+CREATE TABLE coupon_detail (
+                               cpid          VARCHAR2(30 BYTE) NOT NULL,
+                               eno           NUMBER NOT NULL,
+                               memail        VARCHAR2(60 BYTE) NOT NULL,
+                               cpissuedate   TIMESTAMP NOT NULL,
+                               cpexpiredate  TIMESTAMP NOT NULL,
+                               cpusedate     TIMESTAMP,
+                               cpstatus      NUMBER(1)
+);
+
+ALTER TABLE coupon_detail ADD CONSTRAINT coupon_detail_pk PRIMARY KEY ( cpid );
+
+ALTER TABLE coupon_detail ADD CONSTRAINT coupon_detail_memail_un UNIQUE ( memail );
+
+CREATE TABLE event (
+                       eno          NUMBER NOT NULL,
+                       etitle       VARCHAR2(100 BYTE),
+                       econtent     VARCHAR2(200 BYTE),
+                       eissuedate   TIMESTAMP,
+                       eexpiredate  TIMESTAMP,
+                       elimitcount  NUMBER,
+                       ecount       NUMBER,
+                       eimg         VARCHAR2(400 BYTE),
+                       ediscount    NUMBER,
+                       estatus      NUMBER(1) NOT NULL,
+                       edetailimg   VARCHAR2(400),
+                       ecoupontitle VARCHAR2(100)
+);
+
+ALTER TABLE event ADD CONSTRAINT event_pk PRIMARY KEY ( eno );
+
 CREATE TABLE funding (
                          fid             VARCHAR2(100 BYTE) NOT NULL,
                          fstore_name     VARCHAR2(100 BYTE) NOT NULL,
@@ -37,12 +80,12 @@ CREATE TABLE funding (
                          fstory          CLOB NOT NULL,
                          fdate           DATE NOT NULL,
                          fstatus         VARCHAR2(20 BYTE) NOT NULL,
-                         competiton_cid  VARCHAR2(100 BYTE) NOT NULL,
-                         member_memail   VARCHAR2(60 BYTE) NOT NULL,
-                         the_hyundai_tid VARCHAR2(100 BYTE) NOT NULL,
+                         cid  VARCHAR2(100 BYTE) NOT NULL,
+                         memail   VARCHAR2(60 BYTE) NOT NULL,
+                         tid VARCHAR2(100 BYTE) NOT NULL,
                          fstore_score    NUMBER,
                          ffunding_score  NUMBER,
-                         category_ctid   VARCHAR2(30 BYTE) NOT NULL
+                         ctid   VARCHAR2(30 BYTE) NOT NULL
 );
 
 COMMENT ON COLUMN funding.fstatus IS
@@ -55,11 +98,9 @@ ALTER TABLE funding ADD CONSTRAINT funding_pk PRIMARY KEY ( fid );
 CREATE TABLE funding_product (
                                  fpid           VARCHAR2(100 BYTE) NOT NULL,
                                  fpoption       VARCHAR2(100 BYTE) NOT NULL,
-                                 funding_fid    VARCHAR2(100 BYTE) NOT NULL,
+                                 fid    VARCHAR2(100 BYTE) NOT NULL,
                                  fporigin_price NUMBER NOT NULL,
-                                 fpprice        unknown
---  ERROR: Datatype UNKNOWN is not allowed
-    ,
+                                 fpprice       NUMBER NOT NULL,
                                  fptitle        VARCHAR2(100 BYTE) NOT NULL,
                                  fpcontent      VARCHAR2(300 BYTE) NOT NULL
 );
@@ -70,7 +111,7 @@ CREATE TABLE member (
                         memail     VARCHAR2(60 BYTE) NOT NULL,
                         mpassword  VARCHAR2(200 BYTE),
                         mname      VARCHAR2(80 BYTE) NOT NULL,
-                        mphone     VARCHAR2(11 BYTE) ,
+                        mphone     VARCHAR2(30 BYTE),
                         mlogintype VARCHAR2(30 BYTE),
                         menabled   NUMBER(1) NOT NULL,
                         mrole      VARCHAR2(100) NOT NULL,
@@ -92,34 +133,34 @@ CREATE TABLE notice (
 ALTER TABLE notice ADD CONSTRAINT notice_pk PRIMARY KEY ( nid );
 
 CREATE TABLE order_item (
-                            funding_product_fpid VARCHAR2(100 BYTE) NOT NULL,
-                            orders_oid           VARCHAR2(40 BYTE) NOT NULL,
+                            fpid                 VARCHAR2(100 BYTE) NOT NULL,
+                            oid                  VARCHAR2(40 BYTE) NOT NULL,
                             oicount              NUMBER NOT NULL,
                             oitotalprice         NUMBER(8) NOT NULL
 );
 
-ALTER TABLE order_item ADD CONSTRAINT order_item_pk PRIMARY KEY ( funding_product_fpid,
-                                                                  orders_oid );
+ALTER TABLE order_item ADD CONSTRAINT order_item_pk PRIMARY KEY ( fpid,
+                                                                  oid );
 
 CREATE TABLE orders (
                         oid                   VARCHAR2(40 BYTE) NOT NULL,
                         oconsumer             VARCHAR2(10 BYTE) NOT NULL,
                         ophone                VARCHAR2(11 BYTE) NOT NULL,
-                        omemo                 VARCHAR2(60 BYTE),
                         oused_mileage         NUMBER,
                         oorigin_price         NUMBER(8) NOT NULL,
                         oafter_price          NUMBER(8) NOT NULL,
                         ostatus               VARCHAR2(15 BYTE) NOT NULL,
                         odate                 DATE,
-                        member_memail         VARCHAR2(60 BYTE) NOT NULL,
-                        payment_method_pmcode VARCHAR2(30 BYTE) NOT NULL,
-                        qrused_date           DATE
+                        memail                VARCHAR2(60 BYTE) NOT NULL,
+                        pmcode                VARCHAR2(30 BYTE) NOT NULL,
+                        qrused_date           DATE,
+                        cpid                  VARCHAR2(30 BYTE) NOT NULL
 );
 
 COMMENT ON COLUMN orders.ostatus IS
-    'COMPLETE    결제 완료
-WAITING               결제 대기
-REFUND      환불';
+    'COMPLETE	 결제 완료
+WAITING            	결제 대기
+REFUND		환불';
 
 ALTER TABLE orders ADD CONSTRAINT orders_pk PRIMARY KEY ( oid );
 
@@ -132,16 +173,16 @@ CREATE TABLE payment_method (
 ALTER TABLE payment_method ADD CONSTRAINT payment_method_pk PRIMARY KEY ( pmcode );
 
 
---  ERROR: UK name length exceeds maximum allowed length(30)
-ALTER TABLE payment_method ADD CONSTRAINT payment_method_pmmethod_pmcompany_un UNIQUE ( pmmethod,
-                                                                                        pmcompany );
+--  ERROR: UK name length exceeds maximum allowed length(30) 
+ALTER TABLE payment_method ADD CONSTRAINT payment_method_un UNIQUE ( pmmethod,
+                                                                     pmcompany );
 
 CREATE TABLE reply (
                        rscore        NUMBER NOT NULL,
                        rtype         VARCHAR2(20) NOT NULL,
                        rimg_url      VARCHAR2(100 BYTE),
-                       member_memail VARCHAR2(60 BYTE) NOT NULL,
-                       funding_fid   VARCHAR2(100 BYTE) NOT NULL,
+                       memail        VARCHAR2(60 BYTE) NOT NULL,
+                       fid           VARCHAR2(100 BYTE) NOT NULL,
                        rdate         DATE NOT NULL
 );
 
@@ -151,8 +192,8 @@ STORE';
 
 ALTER TABLE reply
     ADD CONSTRAINT reply_pk PRIMARY KEY ( rtype,
-                                          member_memail,
-                                          funding_fid );
+                                          memail,
+                                          fid );
 
 CREATE TABLE the_hyundai (
                              tid   VARCHAR2(100 BYTE) NOT NULL,
@@ -161,57 +202,69 @@ CREATE TABLE the_hyundai (
 
 ALTER TABLE the_hyundai ADD CONSTRAINT the_hyundai_pk PRIMARY KEY ( tid );
 
+ALTER TABLE coupon_detail
+    ADD CONSTRAINT coupon_detail_event_fk FOREIGN KEY ( eno )
+        REFERENCES event ( eno );
+
+ALTER TABLE coupon_detail
+    ADD CONSTRAINT coupon_detail_member_fk FOREIGN KEY ( memail )
+        REFERENCES member ( memail );
+
 ALTER TABLE funding
-    ADD CONSTRAINT funding_category_fk FOREIGN KEY ( category_ctid )
+    ADD CONSTRAINT funding_category_fk FOREIGN KEY ( ctid )
         REFERENCES category ( ctid );
 
 ALTER TABLE funding
-    ADD CONSTRAINT funding_competiton_fk FOREIGN KEY ( competiton_cid )
+    ADD CONSTRAINT funding_competiton_fk FOREIGN KEY ( cid )
         REFERENCES competiton ( cid );
 
 ALTER TABLE funding
-    ADD CONSTRAINT funding_member_fk FOREIGN KEY ( member_memail )
+    ADD CONSTRAINT funding_member_fk FOREIGN KEY ( memail )
         REFERENCES member ( memail );
 
 ALTER TABLE funding_product
-    ADD CONSTRAINT funding_product_funding_fk FOREIGN KEY ( funding_fid )
+    ADD CONSTRAINT funding_product_funding_fk FOREIGN KEY ( fid )
         REFERENCES funding ( fid );
 
 ALTER TABLE funding
-    ADD CONSTRAINT funding_the_hyundai_fk FOREIGN KEY ( the_hyundai_tid )
+    ADD CONSTRAINT funding_the_hyundai_fk FOREIGN KEY ( tid )
         REFERENCES the_hyundai ( tid );
 
 ALTER TABLE order_item
-    ADD CONSTRAINT order_item_funding_product_fk FOREIGN KEY ( funding_product_fpid )
+    ADD CONSTRAINT order_item_funding_product_fk FOREIGN KEY ( fpid )
         REFERENCES funding_product ( fpid );
 
 ALTER TABLE order_item
-    ADD CONSTRAINT order_item_orders_fk FOREIGN KEY ( orders_oid )
+    ADD CONSTRAINT order_item_orders_fk FOREIGN KEY ( oid )
         REFERENCES orders ( oid );
 
 ALTER TABLE orders
-    ADD CONSTRAINT orders_member_fk FOREIGN KEY ( member_memail )
+    ADD CONSTRAINT orders_coupon_detail_fk FOREIGN KEY ( cpid )
+        REFERENCES coupon_detail ( cpid );
+
+ALTER TABLE orders
+    ADD CONSTRAINT orders_member_fk FOREIGN KEY ( memail )
         REFERENCES member ( memail );
 
 ALTER TABLE orders
-    ADD CONSTRAINT orders_payment_method_fk FOREIGN KEY ( payment_method_pmcode )
+    ADD CONSTRAINT orders_payment_method_fk FOREIGN KEY ( pmcode )
         REFERENCES payment_method ( pmcode );
 
 ALTER TABLE reply
-    ADD CONSTRAINT reply_funding_fk FOREIGN KEY ( funding_fid )
+    ADD CONSTRAINT reply_funding_fk FOREIGN KEY ( fid )
         REFERENCES funding ( fid );
 
 ALTER TABLE reply
-    ADD CONSTRAINT reply_member_fk FOREIGN KEY ( member_memail )
+    ADD CONSTRAINT reply_member_fk FOREIGN KEY ( memail )
         REFERENCES member ( memail );
 
 
 
--- Oracle SQL Developer Data Modeler 요약 보고서:
---
--- CREATE TABLE                            11
+-- Oracle SQL Developer Data Modeler 요약 보고서: 
+-- 
+-- CREATE TABLE                            13
 -- CREATE INDEX                             0
--- ALTER TABLE                             24
+-- ALTER TABLE                             29
 -- CREATE VIEW                              0
 -- ALTER VIEW                               0
 -- CREATE PACKAGE                           0
@@ -237,15 +290,15 @@ ALTER TABLE reply
 -- CREATE SYNONYM                           0
 -- CREATE TABLESPACE                        0
 -- CREATE USER                              0
---
+-- 
 -- DROP TABLESPACE                          0
 -- DROP DATABASE                            0
---
+-- 
 -- REDACTION POLICY                         0
---
+-- 
 -- ORDS DROP SCHEMA                         0
 -- ORDS ENABLE SCHEMA                       0
 -- ORDS ENABLE OBJECT                       0
---
+-- 
 -- ERRORS                                   2
 -- WARNINGS                                 0
