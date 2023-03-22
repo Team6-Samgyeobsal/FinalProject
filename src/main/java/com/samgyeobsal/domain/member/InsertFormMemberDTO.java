c7f482959e80bf834b0159202959e6e20e480e83
woowang789 <wangjh789@gmail.com>
2023-03-22 10:26:47 +0900

    [design] mypage

--- src/main/java/com/samgyeobsal/domain/member/InsertFormMemberDTO.java
+++ src/main/java/com/samgyeobsal/domain/member/InsertFormMemberDTO.java
@@ -17,6 +17,7 @@ public class InsertFormMemberDTO {
     @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
 
+    @NotBlank(message = "휴대폰 번호를 입력해주세요.")
     private String phone;
 
     public MemberVO toMember(){
