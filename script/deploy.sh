fcce57aa92c0fbeac0f44f5b1aa32d9fdc83b2f3
Taeseung Choi <96964263+isshosng@users.noreply.github.com>
2023-03-28 15:11:48 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
- 
+
 REPOSITORY=/home/ubuntu
- 
+PROJECT=thechef.site
+
 cd $REPOSITORY
- 
+
 JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
- 
+
 CURRENT_PID=$(pgrep -f *.jar)
- 
+
 if [ -z $CURRENT_PID ]
 then
   echo "> Nothing to end."
@@ -17,6 +18,9 @@ else
   kill -15 $CURRENT_PID
   sleep 5
 fi
- 
+
 echo "> $JAR_PATH deploy"
-nohup java -jar -Dserver.port=8080 $JAR_PATH &
+
+nohup java -jar \
+  -Dspring.config.location=file:///home/ubuntu/$PROJECT/application.properties \
+  $JAR_PATH >> /home/ubuntu/$PROJECT/log/nohup_log.out 2>&1 &
