580ee553c6435097afcb2b263d46c4abbc246a63
woowang789 <wangjh789@gmail.com>
2023-03-31 16:32:03 +0900

    [feat] add s3 bucket

--- src/main/java/com/samgyeobsal/domain/common/UploadImgDTO.java
+++ src/main/java/com/samgyeobsal/domain/common/UploadImgDTO.java
@@ -15,10 +15,16 @@ public class UploadImgDTO implements Serializable {
     private String uuid;
     private String folderPath;
 
+    private boolean isLocal;
+
     public String getImageURL(){
         try{
-            String url = folderPath + "/" + uuid + "_" + fileName;
-            return "http://localhost/api/common/displayImg?imgName="+URLEncoder.encode(url,"UTF-8");
+            if(isLocal){
+                String url = folderPath + "/" + uuid + "_" + fileName;
+                return "http://localhost/api/common/displayImg?imgName="+URLEncoder.encode(url,"UTF-8");
+            }else{
+                return fileName;
+            }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
