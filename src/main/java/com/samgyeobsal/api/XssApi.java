bacf2b3b21cc6ec904a5fad78c5a6bc3be668163
최태승 <96964263+isshosng@users.noreply.github.com>
2023-04-02 20:35:24 +0900

    [update] XssApi

--- src/main/java/com/samgyeobsal/api/XssApi.java
+++ src/main/java/com/samgyeobsal/api/XssApi.java
@@ -6,7 +6,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * @filename XssApi
+ * @author 최태승
+ * @since 2023.04.02
+ * lucy
+ *
+ * <pre>
+ * 수정일        	수정자       			수정내용
+ * ----------  --------    ---------------------------
+ * 2023.04.02	최태승		최초 생성
+ * </pre>
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/test/v1/xss-test")
