e132e694e5cdabf8e5472bba53cdcec1ea4ef332
woowang789 <wangjh789@gmail.com>
2023-04-04 17:20:08 +0900

    [feat] add translate

--- src/main/java/com/samgyeobsal/FinalProjectApplication.java
+++ src/main/java/com/samgyeobsal/FinalProjectApplication.java
@@ -9,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
@@ -23,6 +27,14 @@ public class FinalProjectApplication {
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 		return objectMapper;
+	}
+
+	// 언어 & 국가정보가 없는 경우 한국으로 인식하도록 설정
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
 	}
 
 }
--- src/main/java/com/samgyeobsal/api/ChatGptApi.java
+++ src/main/java/com/samgyeobsal/api/ChatGptApi.java
@@ -1,11 +1,10 @@
 package com.samgyeobsal.api;
 
 import com.lilittlecat.chatgpt.offical.ChatGPT;
-import com.samgyeobsal.service.NaverTransService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.OkHttpClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,22 +20,19 @@ import java.util.concurrent.TimeUnit;
 @Log4j2
 public class ChatGptApi {
 
-    private final NaverTransService naverTransService;
+    @Value("${chatgpt.client.key}")
+    private String chatGPTKey;
 
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<?> chapGpt(String question) throws IOException {
-
-//        String result = naverTransService.getTransSentence(question,"ko","en");
 
         OkHttpClient http = new OkHttpClient.Builder()
                 .connectTimeout(60, TimeUnit.SECONDS)
                 .writeTimeout(60, TimeUnit.SECONDS)
                 .readTimeout(60, TimeUnit.SECONDS)
                 .build();
-        ChatGPT chatGPT = new ChatGPT("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", http);
+        ChatGPT chatGPT = new ChatGPT(chatGPTKey, http);
         String hello = chatGPT.ask(question);
-
-//        String result2=naverTransService.getTransSentence(hello,"en","ko");
 
         return new ResponseEntity<>(hello, HttpStatus.OK);
     }
