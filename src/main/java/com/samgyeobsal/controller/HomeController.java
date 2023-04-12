446d70d85c5deb3a43f4a3e95079ee31b9826a1b
woowang789 <wangjh789@gmail.com>
2023-04-12 15:52:23 +0900

    [design] apply favicon

--- src/main/java/com/samgyeobsal/controller/HomeController.java
+++ src/main/java/com/samgyeobsal/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/web/")
+@RequestMapping("/web")
 @RequiredArgsConstructor
 @Slf4j
 public class HomeController {
